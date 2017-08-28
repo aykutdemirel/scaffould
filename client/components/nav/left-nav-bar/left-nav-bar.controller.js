fd11bcc5a6fc23bd3da6610e9bd368ae75d39fa5
Aykut DEMİREL <ademirel@N106603.local>
2017-08-28 23:31:49 +0300

    updated

--- client/components/nav/left-nav-bar/left-nav-bar.controller.js
+++ client/components/nav/left-nav-bar/left-nav-bar.controller.js
@@ -25,19 +25,7 @@
 
         function getMenu () {
             return [
-                { title: 'Ana Sayfa', link: '/admin/dashboard', route: '/admin/dashboard', authorization:'VIEW_MENU_DASHBOARD' },
-                { title: 'Kullanıcılar', link:'/admin/users', route:'/admin/users', authorization:'VIEW_MENU_USERS' },
-                { title: 'Uygulamalar', link: '/admin/apps', route: '/admin/apps', authorization:'VIEW_MENU_APPS' },
-                { title: 'Uygulama Tipleri', link: '/admin/app-type', route: '/admin/app-type', authorization:'VIEW_MENU_APPTYPE' },
-                { title: 'Cihazlar', link: '/admin/devices', route: '/admin/devices', authorization:'VIEW_MENU_DEVICES' },
-                { title: 'Domainler', link: '/admin/domains', route: '/admin/domains', authorization:'VIEW_MENU_DOMAINS' },
-                { title: 'Yetkiler', link: '/admin/authorization', route: '/admin/authorization', authorization:'VIEW_MENU_AUTHORIZATION' },
-                { title: 'Roller', link: '/admin/roles', route: '/admin/roles', authorization:'VIEW_MENU_ROLES' },
-                { title: 'Alarmlar', link: '/admin/alarms', route: '/admin/alarms', authorization:'VIEW_MENU_ALARMS' },
-                { title: 'Widget Havuzu', link:'/admin/widget', route:'/admin/widget', authorization:'VIEW_MENU_WIDGET' },
-                { title: 'Custom Widget', link:'/admin/custom-widget', route:'/admin/custom-widget', authorization:'VIEW_MENU_WIDGET' },
-                { title: 'Sayfalar', link:'/admin/pages', route:'/admin/pages', authorization:'VIEW_MENU_PAGES' },
-                { title: 'İrtibat', link:'/admin/contacts', route:'/admin/contacts', authorization:'VIEW_MENU_CONTACTS' },
+                { title: 'Ana Sayfa', link: '/dashboard', route: '/dashboard'}
             ]
         }
 
